bd3f3a7554bf1e72e4510598a8608973dbd13708
martin Lindahl <martin.li@glimakra.se>
2021-08-25 11:16:21 +0200

    Hämta data med  useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) => {
+      setState(json)
+    });
+    }, []);
+
+    const [state, setState] = useState(null)
+    console.log('state', state)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {state ? state.map(obj => <li>{obj.id}</li>) : <div>Loading</div>}
       </header>
     </div>
   );
